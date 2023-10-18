b205cd003607dc20417e8c0615e1007c6164e683
trandainamnene <dainamne123@gmail.com>
2023-10-18 22:18:01 +0700

    "dark mode theme"

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,35 @@
-function load(id , url)
-{
-    const tagHTML =  document.getElementById(id)
+function load(id, url) {
+    const tagHTML = document.getElementById(id)
     const cache = localStorage.getItem(url);
     if (cache) {
         tagHTML.innerHTML = cache;
     }
-    fetch(url)
-    .then((data) => data.text())
-    .then((result) => {
-        if (result !== cache) {
-            tagHTML.innerHTML = result;
-        }
-        localStorage.setItem(url , tagHTML.innerHTML);
-    })
+    return fetch(url)
+        .then((data) => data.text())
+        .then((result) => {
+            if (result !== cache) {
+                tagHTML.innerHTML = result;
+            }
+            localStorage.setItem(url, tagHTML.innerHTML);
+        })
 }
+
+// Light + Dark
+async function addEventBtnLightDark() {
+    const switchBtn = document.querySelector('.action__switch');
+    let srcImg = "";
+    switchBtn.onclick = function (e) {
+        if (document.documentElement.classList.toggle("dark")) {
+            srcImg = "./assests/icons/sun.svg";
+        } else {
+            srcImg = "./assests/icons/moon.svg";
+        };
+        console.log(srcImg)
+        switchBtn.querySelector('.icon').setAttribute('src' , srcImg);
+    }
+    console.log(switchBtn.onclick)
+}
+
+load("header", "./templates/header.html")
+    .then(addEventBtnLightDark);
+load("footer", "./templates/footer.html");
